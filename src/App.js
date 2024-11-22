347c110a7d7262cbd4c3ec21924e9dec4798f6bb
Amir Alikhani <alikhaniamir83@gmail.com>
2024-11-22 19:19:06 +0330

    general set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import routes from "./routes";
+import { useRoutes } from "react-router-dom";
+import TopBar from "./components/TopBar/TopBar";
+import SideBar from "./components/SideBar/SideBar";
 
-function App() {
+export default function App() {
+  let mainRoutes = useRoutes(routes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopBar></TopBar>
+      <div className="app-container">
+        <SideBar></SideBar>
+        {mainRoutes}
+      </div>
     </div>
   );
 }
-
-export default App;
