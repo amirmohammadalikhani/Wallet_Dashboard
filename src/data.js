b071c6d53d92e576c228f79edd0d1f3c97a0d0c7
Amir Alikhani <alikhaniamir83@gmail.com>
2024-11-22 19:11:27 +0330

    add informations of users

---
+++ src/data.js
@@ -0,0 +1,52 @@
+let xAxisData = [
+  {
+    month: "Jan",
+    Sale: 112_000,
+  },
+  {
+    month: "Feb",
+    Sale: 99_000,
+  },
+  {
+    month: "Mar",
+    Sale: 12_100,
+  },
+  {
+    month: "Apr",
+    Sale: 90_000,
+  },
+  {
+    month: "May",
+    Sale: 54_000,
+  },
+  {
+    month: "Jun",
+    Sale: 85_000,
+  },
+  {
+    month: "Jul",
+    Sale: 34_000,
+  },
+  {
+    month: "Aug",
+    Sale: 18_568,
+  },
+  {
+    month: "Sep",
+    Sale: 2_193,
+  },
+  {
+    month: "Oct",
+    Sale: 73_074,
+  },
+  {
+    month: "Nov",
+    Sale: 13_900,
+  },
+  {
+    month: "Dec",
+    Sale: 97_000,
+  },
+];
+
+export { xAxisData };
